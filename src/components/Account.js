50b161eeb4600d34d7504c59026deefe3268a1bf
Olivier Berho <olivier@kasual.biz>
2017-06-20 11:18:06 +0200

    [+] add instructions.

--- src/components/Account.js
+++ src/components/Account.js
@@ -17,8 +17,19 @@ class Account extends Component {
                 <h6 className="card-subtitle">Dernières opérations : toutes</h6>
               </div>
               <div className="card-body">
-                <p>Afficher les opérations ici.</p>
-                <p>Charger le contenu du fichier <code>src/config/data.js</code></p>
+                <p>
+                  Afficher les opérations ici<br />
+                  Une opération (ou item) = un titre, une date et un montant en €
+                </p>
+                <p>
+                  Charger le contenu du fichier <code>src/config/data.js</code><br />
+                  Gérer les différents filtres ci-dessous (validée = item.checked === true)<br />
+                  Changer la valeur checked d'un item en cliquant sur un bouton<br />
+                  Utiliser les flexbox pour le positionnement des éléments
+                </p>
+                <p>Le thème utilisé est <a href="https://picturepan2.github.io/spectre/index.html#introduction">spectre.css</a></p>
+                <p>La librairie d'utilitaires proposée est <a href="http://ramdajs.com/docs/#">ramda</a></p>
+                <p>L'affichage des dates est fait avec <a href="https://momentjs.com/">moment</a></p>
               </div>
               <div className="card-footer">
                 <div className="btn-group btn-group-block">
